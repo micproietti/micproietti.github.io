115f1fb69d4a1ed061ccede4e161863a5184160c
micproietti <164628747+micproietti@users.noreply.github.com>
2024-07-26 12:36:32 +0200

    First launch

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // Script JavaScript di esempio
 
-// Mostra un messaggio di benvenuto quando la pagina è caricata
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Benvenuto nel sito di Michele Proietti!");
-});
-
 // Aggiungi un effetto hover ai link di navigazione
 const navLinks = document.querySelectorAll("nav ul li a");
 navLinks.forEach(link => {
